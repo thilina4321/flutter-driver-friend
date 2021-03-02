019522cf75f713f495d7ca8adbea6e7ec8c1c434
DESKTOP-FRK722U\Thilina <dilshanthilina53@gmail.com>
2021-03-03 02:34:29 +0530

    driver appointment and cart

--- lib/helper/bottom-sheet.dart
+++ lib/helper/bottom-sheet.dart
@@ -4,9 +4,42 @@ import 'package:driver_friend/provider/driver_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class CustomBottomSheet {
-  static bool isLoading = false;
-  static _appointment(context) async {
+mixin CustomBottomSheet {
+  static Future bottomSheet(BuildContext context, String name) async {
+    return showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return MybottomSheet(name);
+        });
+  }
+}
+
+class MybottomSheet extends StatefulWidget {
+  final String name;
+  const MybottomSheet(this.name);
+
+  @override
+  _MybottomSheetState createState() => _MybottomSheetState();
+}
+
+class _MybottomSheetState extends State<MybottomSheet> {
+  DateTime pickedDate;
+  TimeOfDay pickedTime;
+  bool isLoading = false;
+
+  pickDateMethod(date) {
+    setState(() {
+      pickedDate = date;
+    });
+  }
+
+  pickTimeMethod(time) {
+    setState(() {
+      pickedTime = time;
+    });
+  }
+
+  _appointment(context) async {
     print(pickedDate.toString());
     print(pickedTime.format(context));
     isLoading = true;
@@ -21,62 +54,81 @@ class CustomBottomSheet {
     // }
   }
 
-  static DateTime pickedDate;
-  static TimeOfDay pickedTime;
-
-  static Future bottomSheet(BuildContext context, String name) async {
-    return showModalBottomSheet(
-        context: context,
-        builder: (context) {
-          return Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Appointment',
-                  style: TextStyle(fontSize: 25, color: Colors.purple),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Text(name),
-                FlatButton(
-                  onPressed: () {
-                    return showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime.now(),
-                      lastDate: DateTime.now().add(
-                        Duration(days: 7),
-                      ),
-                    ).then((date) => {pickedDate = date});
-                  },
-                  child: Text('Select Date'),
-                ),
-                FlatButton(
-                  onPressed: () {
-                    return showTimePicker(
-                      context: context,
-                      initialTime: TimeOfDay(hour: 00, minute: 00),
-                    ).then((time) => {pickedTime = time});
-                  },
-                  child: Text('Select Time'),
-                ),
-                RaisedButton(
-                  color: Colors.purple,
-                  onPressed: () => _appointment(context),
-                  child: isLoading
-                      ? CircularProgressIndicator()
-                      : Text(
-                          'Make the Appointment',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                ),
-              ],
-            ),
-          );
-        });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Appointment',
+            style: TextStyle(fontSize: 25, color: Colors.purple),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Text(widget.name),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (pickedDate != null)
+                Text(pickedDate.year.toString() +
+                    '/' +
+                    pickedDate.month.toString() +
+                    '/' +
+                    pickedDate.day.toString()),
+              FlatButton(
+                onPressed: () {
+                  return showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime.now().add(
+                      Duration(days: 7),
+                    ),
+                  ).then((date) => {pickDateMethod(date)});
+                },
+                child: pickedDate != null
+                    ? Text('Change Date')
+                    : Text('Select Date'),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (pickedTime != null)
+                Text(pickedTime.hour.toString() +
+                    ':' +
+                    pickedTime.minute.toString()),
+              FlatButton(
+                onPressed: () {
+                  return showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay(hour: 00, minute: 00),
+                  ).then((time) => {pickTimeMethod(time)});
+                },
+                child: pickedTime != null
+                    ? Text('Change Time')
+                    : Text('Select Time'),
+              ),
+            ],
+          ),
+          RaisedButton(
+            color: Colors.purple,
+            onPressed: () => _appointment(context),
+            child: isLoading
+                ? CircularProgressIndicator()
+                : Text(
+                    'Make the Appointment',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/helper/sheet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sheet extends StatefulWidget {
+  static String routeName = 'sheet-screen-bottom';
+
+  @override
+  _SheetState createState() => _SheetState();
+}
+
+class _SheetState extends State<Sheet> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
